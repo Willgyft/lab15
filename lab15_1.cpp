b409b6f1aa0a369d2d34659cf267a386fbc16702
Willgyft <warintorn_sr@cmu.ac.th>
2026-02-03 14:22:50 +0700

    lab15_1.cpp complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	void *x = &a;
+	void *y = &b;
+	void **z = &x;
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+		cout << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z << "\n";
+	c = 'F';
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	*(char*)y = 'W';
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	*(int*)x = 6;
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
+	**(int**)z = 7;
+		cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << "\n";
 
-	
-	
 	return 0;
 }
